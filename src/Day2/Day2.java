5b03ffec9ba0c037d67e891bdf61568aae4e8951
joybob22 <braydenlemke8@gmail.com>
2024-12-02 15:58:58 -0700

    Modified part 2 day 2 to be simpler

--- src/Day2/Day2.java
+++ src/Day2/Day2.java
@@ -5,12 +5,17 @@ import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Day2 {
 
 	public static void main(String[] args) {
 		System.out.println("Safe Reports Part 1: " + part1());
-		System.out.println("Safe Reports Part 2: " + part2());
+		try {
+			System.out.println("Safe Reports Part 2 better: " + part2());
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
 
 	}
 	
@@ -50,77 +55,21 @@ public class Day2 {
 		return safeReports;
 	}
 	
-	public static int part2() {
-		int safeReports = 0;
-		try {
-            String input = Files.readString(Path.of("src/Day2/Day2Input.txt"));
-            String[] reports = input.split("\n");
-            for(String report : reports) {
-            	ArrayList<Integer> levels = Arrays.asList(report.split(" ")).stream().map(Integer::parseInt).collect(Collectors.toCollection(ArrayList::new));
-            	boolean isSafe = true;
-            	boolean levelRemoved = false;
-            	
-            	Direction direction;
-            	if (levels.get(0) < levels.get(1)) {
-            		direction = Direction.POSITIVE;
-            	} else if (levels.get(0) > levels.get(1)) {
-            		direction = Direction.NEGATIVE;
-            	} else if (canRemoveWithSafeRunThrough(levels, 0)) {
-    				safeReports++;
-    				continue;
-    			} else {
-    				continue;
-    			}
-            	
-            	for(int i = 0; i < levels.size() - 1; i++) {
-            		int distance = direction == Direction.POSITIVE ? levels.get(i+1) - levels.get(i) : levels.get(i) - levels.get(i+1);
-            		if (distance < 1 || distance > 3) {
-            			if(canRemoveWithSafeRunThrough(levels, i)) {
-            				break;
-            			} else {
-            				isSafe = false;
-            				break;
-            			}
-            		}
-            	}
-            	
-            	if(isSafe) {
-            		safeReports++;
-            	}
-            }
-            
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-		
-		return safeReports;
-	}
-	
-	public static boolean canRemoveWithSafeRunThrough(ArrayList<Integer> levels, int i) {
-		Integer temp;
-		if(i > 0) {
-			temp = levels.get(i-1);
-			levels.remove(i-1);
-			if(isSafeRunThrough(levels)) {
-				return true;
-			}
-			levels.add(i-1, temp);
-		}
-		
-		temp = levels.get(i);
-		levels.remove(i);
-		if (isSafeRunThrough(levels)) {
-			return true;
-		}
-		levels.add(i, temp);
-
-		temp = levels.get(i+1);
-		levels.remove(i+1);
-		if(isSafeRunThrough(levels)) {
-			return true;
-		}
-		
-		return false;
+	public static int part2() throws Exception {
+        String input = Files.readString(Path.of("src/Day2/Day2Input.txt"));
+        String[] reports = input.split("\n");
+        
+        return (int) Stream.of(reports).map((String report) -> {
+        	ArrayList<Integer> levels = Arrays.asList(report.split(" ")).stream().map(Integer::parseInt).collect(Collectors.toCollection(ArrayList::new));
+        	for(int i = 0; i < levels.size(); i++) {
+        		int temp = levels.get(i);
+        		levels.remove(i);
+        		if(isSafeRunThrough(levels))
+        			return 1;
+        		levels.add(i, temp);
+        	}
+        	return 0;
+        }).reduce(0, (a, b) -> a + b); 
 	}
 	
 	public static boolean isSafeRunThrough(ArrayList<Integer> levels) {
